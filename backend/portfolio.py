5364f0ceaea30a7d6020239ade28284110f44973
BoyTheTall <lungelowmhlanga@gmail.com>
2025-03-23 22:25:37 +0200

    adding some attributes to the portfolio class

--- backend/portfolio.py
+++ backend/portfolio.py
@@ -1,6 +1,17 @@
 class portfolio:
     current_value = None
     money_deposited = None
+    date =  None
+    gain = None
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self, current_value, money_deposited, date) -> None:
+        self.current_value = current_value
+        self.money_deposited = money_deposited
+        self.date = date
+        self.gain = current_value - money_deposited
+        
+    def getCurrentValue(self):
+        return self.current_value
+    
+    def getMoneyDeposited(self):
+        return self.money_deposited
